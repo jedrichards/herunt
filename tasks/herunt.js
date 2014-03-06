5b3d5ab63b432879b4e24fae31e864d90e28a707
Jurie-Jan Botha <juriejanbotha@gmail.com>
2014-03-06 22:23:05 +0200

    Added option for including linked modules and excluding repo folder

--- tasks/herunt.js
+++ tasks/herunt.js
@@ -21,6 +21,7 @@ module.exports = function (grunt) {
         var exclude = this.data.exclude||[];
         var config = this.data.config;
         var name = this.data.name;
+        var includeModules = this.data.includeModules||[];
 
         app = app.charAt(app.length-1) === "/" ? app.slice(0,-1) : app;
         herokuRepo = herokuRepo.charAt(herokuRepo.length-1) === "/" ? herokuRepo.slice(0,-1) : herokuRepo;
@@ -219,12 +220,29 @@ module.exports = function (grunt) {
 
         function syncAppToHerokuRepo (cb) {
             grunt.log.write("Syncing app files to herokuRepo ");
+            var includeCompiled = [];
+            var excludeCompiled = _.union(exclude, [
+                ".DS_Store",
+                "node_modules/*",
+                ".git",
+                ".gitignore",
+                ".nodemonignore",
+                "npm-debug.log",
+                herokuRepo
+            ]);
+            _.each(includeModules, function (module) {
+                var modulePath = "node_modules/" + module,
+                    modulePathSub = modulePath + "/node_modules";
+                includeCompiled.push(modulePath);
+                excludeCompiled.push(modulePathSub);
+            });
             rsync({
                 src: app+"/",
                 dest: herokuRepo,
                 recursive: true,
-                exclude: _.union(exclude,[".DS_Store","node_modules",".git",".gitignore",".nodemonignore","npm-debug.log"]),
-                args: ["--delete"]
+                exclude: excludeCompiled,
+                include: includeCompiled,
+                args: ["--delete", "--copy-links"]
             }, function (err,stdout,stderr,cmd) {
                 if ( err) {
                     cb("Unable to sync app files to herokuRepo. "+err+" "+stdout+" "+stderr);
